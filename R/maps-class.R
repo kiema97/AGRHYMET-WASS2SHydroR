e3db07ba4f650cf7cca5a8333fbfea59cb842e5c
kiema97 <statplus.dec@gmail.com>
2026-02-26 06:27:20 +0000

    updating tune and map-class

--- R/maps-class.R
+++ R/maps-class.R
@@ -152,7 +152,7 @@ plot_class_map <- function(
         above  = colors$above,
         climatology = colors$climatology
       ),
-      drop = TRUE,
+      drop = FALSE,
       na.value = colors$climatology,
       name = "Class"
     )
