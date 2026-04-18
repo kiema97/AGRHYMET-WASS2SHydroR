2c2c8f9c7ea88227b3c8dd9783fb69b84eaa2049
kiema97 <statplus.dec@gmail.com>
2026-04-18 13:02:41 +0100

    adding new function wass2s_detect_high_flow_periods

--- R/maps-class.R
+++ R/maps-class.R
@@ -152,7 +152,7 @@ plot_class_map <- function(
         above  = colors$above,
         climatology = colors$climatology
       ),
-      drop = FALSE,
+      drop = TRUE,
       na.value = colors$climatology,
       name = "Class"
     )
